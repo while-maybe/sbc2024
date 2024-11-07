5de3030536ef70b1e56b17877196936ecd2e8e2f
username <178353419+while-maybe@users.noreply.github.com>
2024-11-07 16:06:43 +0000

    feat: add models+urls data+superuser

--- week-8/todo_list/todolist/admin.py
+++ week-8/todo_list/todolist/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Task
+
+# this enables the model to be managed by the admin site
+admin.site.register(Task)
--- week-8/todo_list/todolist/models.py
+++ week-8/todo_list/todolist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100) # task title
+    description = models.TextField() # task description
+    completed = models.BooleanField() # task status
+    
+    def __str__(self):
+        return self.title
+    
